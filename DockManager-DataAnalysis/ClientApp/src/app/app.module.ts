56a799f4f5e3eba021d93d82be5ece951fc680fb
HristoP96 <HPopov@infragistics.com>
2020-07-15 14:22:02 +0300

    Initial commit

---
+++ DockManager-DataAnalysis/ClientApp/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+@NgModule({
+  declarations: [
+    AppComponent,
+    FloatingPanesService,
+    ConditionalFormattingDirective
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class AppModule {
+}
