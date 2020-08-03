90f4fd04ce05ac295c5387e62aeb29f0f80b9a41
hpopov <Hpopov@infragistics.com>
2020-08-03 13:04:18 +0300

    add SPA config

--- MarketingDashboard/Startup.cs
+++ MarketingDashboard/Startup.cs
@@ -23,10 +23,10 @@ namespace MarketingDashboard
         {
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
-            //services.AddSpaStaticFiles(configuration =>
-            //{
-            //    configuration.RootPath = "ClientApp/dist";
-            //});
+            services.AddSpaStaticFiles(configuration =>
+            {
+               configuration.RootPath = "ClientApp/dist";
+            });
 
 
 
@@ -73,20 +73,20 @@ namespace MarketingDashboard
             });
 
 
-            //app.UseSpa(spa =>
-            //{
-            //    // To learn more about options for serving an Angular SPA from ASP.NET Core,
-            //    // see https://go.microsoft.com/fwlink/?linkid=864501
+            app.UseSpa(spa =>
+            {
+               // To learn more about options for serving an Angular SPA from ASP.NET Core,
+               // see https://go.microsoft.com/fwlink/?linkid=864501
 
-            //    spa.Options.SourcePath = "ClientApp";
+               spa.Options.SourcePath = "ClientApp";
 
-            //    if (env.IsDevelopment())
-            //    {
-            //        spa.UseAngularCliServer(npmScript: "start");
-            //        spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
+               if (env.IsDevelopment())
+               {
+                   spa.UseAngularCliServer(npmScript: "start");
+                   spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
 
-            //    }
-            //});
+               }
+            });
         }
     }
 }
